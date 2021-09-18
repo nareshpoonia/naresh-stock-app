c5d21e2498e0acfad97a5cff724f426ecbae9852
Naresh <68911787+nareshpoonia@users.noreply.github.com>
2021-09-18 12:41:41 +0530

    completed javascript

---
+++ index.js
@@ -0,0 +1,39 @@
+var initialPrice = document.querySelector(".initial-price");
+var quantity = document.querySelector(".quantity");
+var currentPrice = document.querySelector(".current-price");
+var tellMeButton = document.querySelector(".tell-me-button");
+var output = document.querySelector(".output");
+
+tellMeButton.addEventListener("click", clickHandler);
+
+var ip = Number(initialPrice.value);
+var qty = Number(quantity.value);
+var price = Number(currentPrice.value);
+
+function clickHandler() {
+  calculateProfitAndLoss(ip, qty, price);
+}
+
+function calculateProfitAndLoss(ip, qty, price) {
+  var profit = price - ip;
+  var loss = ip - price;
+  var profitPercentage = (profit / price) * 100;
+  var lossPercentage = (loss / price) * 100;
+  if (ip > price) {
+    output.innerText =
+      "Sorry, You have made a loss of " +
+      loss +
+      ". The loss percentage is " +
+      lossPercentage.toFixed(2) +
+      "%";
+  } else if (ip === price) {
+    output.innerText = "No profit, No loss";
+  } else {
+    output.innerText =
+      "Yay, You have made a profit of INR " +
+      profit +
+      ". The profit percentage is " +
+      profitPercentage.toFixed(2) +
+      "%.";
+  }
+}
